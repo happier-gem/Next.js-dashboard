66b3e73d55fcd8926647fad229e7ea51eec1bfe0
happier-gem <gamellahappiness@gmail.com>
2026-07-08 16:42:14 +0200

    feat: refactor LatestInvoices component to fetch data internally and add loading skeleton

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,15 +2,11 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import {
-  fetchLatestInvoices,
-  fetchCardData,
-} from '@/app/lib/data';
+import { fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { RevenueChartSkeleton, LatestInvoicesSkeleton } from '@/app/ui/skeletons';
  
-export default async function Page() {
-  const latestInvoices = await fetchLatestInvoices();
+export default async function page() {
   const {
     numberOfInvoices,
     numberOfCustomers,
@@ -37,9 +33,12 @@ export default async function Page() {
        <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
 }
+
+       
