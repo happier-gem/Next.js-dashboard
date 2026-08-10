20d0abc8759d452ba1fbf8e97a8bf58b590787a2
happier-gem <gamellahappiness@gmail.com>
2026-08-10 13:58:27 +0200

    feat: refactor handleSearch function to use useDebouncedCallback for improved performance

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,13 +2,14 @@
  
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';  
  
 export default function Search() {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const router = useRouter();
  
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
 
     const params = new URLSearchParams(searchParams);
@@ -18,7 +19,7 @@ export default function Search() {
       params.delete('query');
     }
     router.replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
  
   return (
     <div className="relative flex flex-1 shrink-0">
