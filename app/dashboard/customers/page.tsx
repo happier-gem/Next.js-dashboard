1d21bb82836231f045c6ddc6be05fa0eec352cf0
happier-gem <gamellahappiness@gmail.com>
2026-08-12 11:02:59 +0200

    feat: add metadata configuration for Dashboard and Customers pages

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,10 @@
 import CustomersTable from '@/app/ui/customers/table';
 import { fetchFilteredCustomers } from '@/app/lib/data';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Customers | Acme Dashboard',
+};
 
 export default async function Page({
   searchParams,
